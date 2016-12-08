6b6030172837da5ef35aef18f751e09b98f4fd00
Alayna <belf0018@algonquinlive.com>
2016-12-08 14:41:44 -0500

    Solar System done w/ skybox and moon

---
+++ CST 8236 Project 3  - Solar System/Assets/Orbit.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour {
+
+    public float siderealPeriod;
+
+    public bool retrograde;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotateDirection;
+        if (retrograde)
+        {
+            rotateDirection = -1.0f;
+        }
+        else {
+            rotateDirection = 1.0f;
+        }
+
+        //orbit around sun based on speed input
+        transform.RotateAround(transform.parent.position, Vector3.up, Time.deltaTime/siderealPeriod*10.0f);
+        //rotate on axis
+        transform.Rotate(new Vector3(10, 20, 5), Time.deltaTime*rotateDirection*10.0f);
+
+    }
+}
